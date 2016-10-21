5ebbeff6d97af281d4ee7887e06bed473b0634da
marianaevv <mariana2v@hotmail.com>
2016-10-20 21:31:46 -0500

    Update CircularList.java

--- src/ED/CircularList.java
+++ src/ED/CircularList.java
@@ -16,42 +16,53 @@ public class CircularList<T> {
     public CircularList() {
         pivot = null;
     }
-
+    }
+/**
+ * Método para insertar nodos
+ * @param d 
+ */
     public void insert(T d) {
-        SimpleNode node = new SimpleNode(d);
+        SimpleNode node = new SimpleNode(d); //Creación de un objeto de tipo nodo simple 
         if (!isEmpty()) {//Si no esta vacia
             //Insertamos nodo al inicio y recorremos el primero
             node.next = pivot;
             findLast().next = node;
             pivot = node;
 
-        } else {
+        } else { //Si esta vacia
+           //Se crea un nuevo nodo el cual apuntara a pivote
             pivot = node;
             node.next = pivot;
         }
     }
-
+/**
+* Método para verificar si la pila esta vacia
+*/
     private boolean isEmpty() {
         return pivot == null;
     }
-
+/**
+* Método  para encontrar el ultimo nodo de la lista
+*/
     private SimpleNode findLast() {
-        SimpleNode aux = pivot;
-        do {
-            aux = aux.next;
+        SimpleNode aux = pivot; //Se crea variable auxiliar de tipo nodo simple y tendrá el valor de pivote
+        do {//Ciclo do while que recorrera la lista
+            aux = aux.next;// La variable auxiliar seguira avanzando hasta que el que siga de aux sea otra vez pivote
         } while (aux.next != pivot);
-        return aux;
+        return aux;//regresamos la variable aux;
     }
-
+/**
+*Método para mostrar la lista
+*/
     public void showList() {
-        SimpleNode aux;//declaramos dos variables auxiliares tipo nodeDL para guardar los valores de nuestros nodos
-        aux = pivot;// aux1 sera igual a first  
-        if (isEmpty()) {//Preguntamos si la lista esta llenanodeClass aux;//declaramos dos variables auxiliares tipo nodeDL para guardar los valores de nuestros nodos
+        SimpleNode aux;//declaramos una variable auxiliar de tipo node simple para guardar los valores de nuestros nodos
+        aux = pivot;// aux será igual a pivote
+        if (isEmpty()) {//Preguntamos si la lista esta vacia
             System.out.println("La lista está vacia");
 
-        } else {
+        } else {// sino 
 
-            do {
+            do {//Imprimira cada dato contenido en los nodos hasta que el siguiente del auxiliar sea igual a pivote.
                 System.out.print("[" + aux.getData() + "]" + "->");
                 aux = aux.next;
             } while (aux.next != pivot.next);
@@ -78,16 +89,21 @@ public class CircularList<T> {
         }
 
     }
-
+/**
+*Método para borrar la información de algún nodo
+*/
     public void eNode(T data) {
-        SimpleNode aux;
-        aux = pivot;
-        while (aux.next != pivot && aux.data != data) {
+        SimpleNode aux; //variable auxiliar de tipo nodo simple
+        aux = pivot;// le damos a la variable el valor de pivote
+        while (aux.next != pivot && aux.data != data) {/*Mientras el valor siguiente del aux sea diferente de pivote
+            y la data de aux no coincida con la data requerida ira recorriendo la lista.*/
             aux = aux.next;
         }
+        //Al salir del ciclo preguntamos si el valor que sigue es igual a pivote, si es verdad significa que la data no fue encontrada 
         if (aux.next == pivot) {
             System.out.println("No se encuentra el nodo");
         }
+        //Si la data del auxiliar es igual a la que queremos eliminar simplemente nos brincaremos ese nodo y asi lo desaparecemos.
         if (aux.data == data) {
             aux.next = aux.next.next;
         }
